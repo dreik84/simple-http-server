2f308d43438fc83a258623e5b533fc0d7229d51f
dreik84 <dreik84@mail.ru>
2025-11-02 17:03:05 +0300

    Update GsonApp.java

--- src/main/java/org/example/GsonApp.java
+++ src/main/java/org/example/GsonApp.java
@@ -1,19 +1,43 @@
 package org.example;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Map;
 
 public class GsonApp {
     public static void main(String[] args) {
         Owner owner = new Owner("John", "Smith", 1111);
         Dog dog = new Dog("Jack", 5, owner);
-        Gson gson = new Gson();
+//        Gson gson = new Gson();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
 
         String jsonString = gson.toJson(dog);
 
         System.out.println(jsonString);
 
+//        jsonString = "{\"name\":\"Tuzik\",\"age\":7,\"owner\":{\"firstName\":\"Tom\",\"lastName\":\"Hanks\",\"phoneNumber\":2222}}";
+        jsonString = """
+                {
+                    "name": "Tuzik",
+                    "age": 7,
+                    "owner": {
+                            "firstName": "Tom",
+                            "lastName": "Hanks",
+                            "phoneNumber": 2222
+                        }
+                }
+                """;
+
+
         dog = gson.fromJson(jsonString, Dog.class);
+        Map map = gson.fromJson(jsonString, Map.class);
 
         System.out.println(dog);
+        System.out.println(map);
     }
 }
