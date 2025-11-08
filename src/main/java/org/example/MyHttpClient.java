e87b0fe55d2073cbe5c3e60f75672266a1a2b4f1
dreik84 <dreik84@mail.ru>
2025-11-08 17:01:51 +0300

    Create MyHttpClient.java

---
+++ src/main/java/org/example/MyHttpClient.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MyHttpClient {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        URI uri = URI.create("https://httpstatuses.com/418");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "text/html")
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+
+        HttpResponse<String> response = client.send(request, handler);
+
+        System.out.println("Код ответа: " + response.statusCode());
+        System.out.println("Тело ответа: " + response.body());
+    }
+}
