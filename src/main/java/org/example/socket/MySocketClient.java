23beb91c388d0744d8ca4b1742040030833b2eb0
dreik84 <dreik84@mail.ru>
2025-11-18 17:01:05 +0300

    Create MySocketClient.java

---
+++ src/main/java/org/example/socket/MySocketClient.java
@@ -0,0 +1,60 @@
+package org.example.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MySocketClient {
+
+    private static String domain;
+
+    private static int port;
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void start() {
+        boolean correctPort = false;
+        boolean correctDomain = false;
+
+        System.out.print("___Настройка клиента самого крутого чата___\n\n");
+
+        while (!correctDomain) {
+            System.out.print("Введите домен сервера: ");
+
+            try {
+                domain = reader.readLine();
+            } catch (IOException e) {
+                System.out.println("[ERROR] Че-то пошло не так, давай еще раз");
+                continue;
+            } catch (Exception e) {
+                System.out.println("[ERROR] Ошибка");
+                continue;
+            }
+
+            correctDomain = true;
+        }
+
+        while (!correctPort) {
+            System.out.print("Введите порт: ");
+
+            try {
+                port = Integer.parseInt(reader.readLine());
+            } catch (IOException e) {
+                System.out.println("[ERROR] Че-то пошло не так, давай еще раз");
+                continue;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] Будь добр, введи цифру");
+                continue;
+            } catch (Exception e) {
+                System.out.println("[ERROR] Ошибка");
+                continue;
+            }
+
+            correctPort = true;
+        }
+
+        try {
+            // TODO
+        }
+    }
+}
